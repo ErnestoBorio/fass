e80e102d29e212a873dd541b40a28d8a00c830e2
Ernesto Borio <ernestoborio@gmail.com>
2019-02-17 00:30:24 -0300

    Cleanup and fresh start.

--- fass.py
+++ fass.py
@@ -3,26 +3,26 @@ from sys import argv, stdout
 from antlr4 import *
 from fassLexer import fassLexer
 from fassParser import fassParser
-from fassCompiler import fassCompiler
+from myParser import myParser
+from myListener import myListener
 
 def main(argv):
 	if len(argv) <= 1:
 		print("Usage: py fass.py source_file.fass binary_file.prg")
 		exit()
+	
 	input = FileStream(argv[1])
 	
-	tree = fassParser( 
-		CommonTokenStream(
-			fassLexer(input))).program()
-	
-	fass = fassCompiler()
-	
-	ParseTreeWalker().walk( fass, tree )
+	lexer = fassLexer( input)
+	tokenStream = CommonTokenStream( lexer)
+	tree = myParser( tokenStream).program()
+	# listener = myListener()
+	# ParseTreeWalker().walk( listener, tree )
 
 	# WIP TODO: implement argparse
 	if len(argv) >= 3:
 		out = open( argv[2], "wb" )
-		out.write( fass.get_output() )
+		out.write( tree.get_output() )
 		out.close()
 
 if __name__ == '__main__':

9ae7fd0f8196631535c18e854d02281e8ecf9c21
Ernesto Borio <ernestoborio@gmail.com>
2022-01-23 23:38:55 -0300

    First Javascript version. npm and project setup.

---
+++ src/fassBaseListener.js
@@ -0,0 +1,486 @@
+// Generated from /Users/petruza/Source/Drean64/fass/src/fass.g4 by ANTLR 4.9.1
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete listener for a parse tree produced by fassParser.
+export default class fassListener extends antlr4.tree.ParseTreeListener {
+
+	// Enter a parse tree produced by fassParser#program.
+	enterProgram(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#program.
+	exitProgram(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#statement.
+	enterStatement(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#statement.
+	exitStatement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#address_hex.
+	enterAddress_hex(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#address_hex.
+	exitAddress_hex(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#address_dec.
+	enterAddress_dec(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#address_dec.
+	exitAddress_dec(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#address_stmt.
+	enterAddress_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#address_stmt.
+	exitAddress_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#remote_label_stmt.
+	enterRemote_label_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#remote_label_stmt.
+	exitRemote_label_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#filler_value.
+	enterFiller_value(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#filler_value.
+	exitFiller_value(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#filler_default.
+	enterFiller_default(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#filler_default.
+	exitFiller_default(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#nop_brk_stmt.
+	enterNop_brk_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#nop_brk_stmt.
+	exitNop_brk_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#const_stmt.
+	enterConst_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#const_stmt.
+	exitConst_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#data_stmt.
+	enterData_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#data_stmt.
+	exitData_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#flag_set_stmt.
+	enterFlag_set_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#flag_set_stmt.
+	exitFlag_set_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#stack_stmt.
+	enterStack_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#stack_stmt.
+	exitStack_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#return.
+	enterReturn(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#return.
+	exitReturn(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#retint.
+	enterRetint(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#retint.
+	exitRetint(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#goto_stmt.
+	enterGoto_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#goto_stmt.
+	exitGoto_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#gosub_stmt.
+	enterGosub_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#gosub_stmt.
+	exitGosub_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#assign_reg_lit.
+	enterAssign_reg_lit(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#assign_reg_lit.
+	exitAssign_reg_lit(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#assign_reg_reg.
+	enterAssign_reg_reg(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#assign_reg_reg.
+	exitAssign_reg_reg(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#assign_reg_ref.
+	enterAssign_reg_ref(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#assign_reg_ref.
+	exitAssign_reg_ref(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#assign_ref_reg.
+	enterAssign_ref_reg(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#assign_ref_reg.
+	exitAssign_ref_reg(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#assign_ref_reg_lit.
+	enterAssign_ref_reg_lit(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#assign_ref_reg_lit.
+	exitAssign_ref_reg_lit(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#assign_ref_reg_ref.
+	enterAssign_ref_reg_ref(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#assign_ref_reg_ref.
+	exitAssign_ref_reg_ref(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#arithmetic_a_lit.
+	enterArithmetic_a_lit(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#arithmetic_a_lit.
+	exitArithmetic_a_lit(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#arithmetic_a_ref.
+	enterArithmetic_a_ref(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#arithmetic_a_ref.
+	exitArithmetic_a_ref(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#arithmetic_reg_inc.
+	enterArithmetic_reg_inc(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#arithmetic_reg_inc.
+	exitArithmetic_reg_inc(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#arithmetic_ref_lit.
+	enterArithmetic_ref_lit(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#arithmetic_ref_lit.
+	exitArithmetic_ref_lit(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#bit_shift_stmt.
+	enterBit_shift_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#bit_shift_stmt.
+	exitBit_shift_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#logic_stmt.
+	enterLogic_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#logic_stmt.
+	exitLogic_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#compare_stmt.
+	enterCompare_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#compare_stmt.
+	exitCompare_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#bittest_stmt.
+	enterBittest_stmt(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#bittest_stmt.
+	exitBittest_stmt(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#label.
+	enterLabel(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#label.
+	exitLabel(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#reference.
+	enterReference(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#reference.
+	exitReference(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#register.
+	enterRegister(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#register.
+	exitRegister(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#reg_axys.
+	enterReg_axys(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#reg_axys.
+	exitReg_axys(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#name.
+	enterName(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#name.
+	exitName(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#indexed.
+	enterIndexed(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#indexed.
+	exitIndexed(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#indir_x.
+	enterIndir_x(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#indir_x.
+	exitIndir_x(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#indir_y.
+	enterIndir_y(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#indir_y.
+	exitIndir_y(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#indirect.
+	enterIndirect(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#indirect.
+	exitIndirect(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#value.
+	enterValue(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#value.
+	exitValue(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#constant.
+	enterConstant(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#constant.
+	exitConstant(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#literal.
+	enterLiteral(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#literal.
+	exitLiteral(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#hex_bigend.
+	enterHex_bigend(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#hex_bigend.
+	exitHex_bigend(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#dec_bigend.
+	enterDec_bigend(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#dec_bigend.
+	exitDec_bigend(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#bin_bigend.
+	enterBin_bigend(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#bin_bigend.
+	exitBin_bigend(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#hex_litend.
+	enterHex_litend(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#hex_litend.
+	exitHex_litend(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#dec_litend.
+	enterDec_litend(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#dec_litend.
+	exitDec_litend(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#bin_litend.
+	enterBin_litend(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#bin_litend.
+	exitBin_litend(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#brk_literal.
+	enterBrk_literal(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#brk_literal.
+	exitBrk_literal(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#nop_literal.
+	enterNop_literal(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#nop_literal.
+	exitNop_literal(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#string.
+	enterString(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#string.
+	exitString(ctx) {
+	}
+
+
+	// Enter a parse tree produced by fassParser#negative_number.
+	enterNegative_number(ctx) {
+	}
+
+	// Exit a parse tree produced by fassParser#negative_number.
+	exitNegative_number(ctx) {
+	}
+
+
+
+}

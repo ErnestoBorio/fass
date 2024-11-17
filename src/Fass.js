1012ce7cff96439777ee24f22d716696879677b1
Ernesto Borio <ernestoborio@gmail.com>
2024-11-17 13:29:48 -0300

    WIP

--- src/Fass.js
+++ src/Fass.js
@@ -1,9 +1,57 @@
 import nearley from "nearley";
 import grammar from "./fassGrammar";
 
+/*
 const compiled = nearley.Grammar.fromCompiled(grammar);
 const parser = new nearley.Parser(compiled, { keepHistory: true });
 parser.feed("$D020");
 
 const ast = parser.results[0];
 console.log(JSON.stringify(ast, null, 2));
+
+import { writeFileSync } from "fs";
+import { readFileSync } from "fs";
+import parseArgv from "minimist";
+---
+const argv = parseArgv(process.argv.slice(2));
+const source = readFileSync(argv["_"][0]).toString();
+const parser = compile(source);
+new Fass().visit(parser.program());
+
+if (argv["o"]) {
+	writeFileSync(argv["o"], fass.output);
+} else {
+	process.stdout.write(fass.output);
+}
+//*/
+
+class FassParser {}
+
+export class FassCompiler {
+	/**
+	 * Compiles the Fass grammar and returns it
+	 * @returns {Grammar}
+	 */
+	compile() {
+		return nearley.Grammar.fromCompiled(grammar);
+	}
+
+	/**
+	 * Creates a parser for the given Fass grammar
+	 * @param {Grammar} compiled
+	 * @returns {Parser}
+	 */
+	parser(compiled) {
+		return new nearley.Parser(compiled, { keepHistory: true });
+	}
+
+	/**
+	 * Parses the given source code
+	 * @param {string | Buffer} source
+	 */
+	run(source) {
+		if (typeof source === "object") {
+			source = source.toString();
+		}
+	}
+}
--- src/fassGrammar.js
+++ src/fassGrammar.js
@@ -1,21 +1,41 @@
 // Generated automatically by nearley, version 2.20.1
 // http://github.com/Hardmath123/nearley
 (function () {
-function id(x) { return x[0]; }
-var grammar = {
-    Lexer: undefined,
-    ParserRules: [
-    {"name": "main", "symbols": ["statement"], "postprocess": d => ({rule: "main", children: d })},
-    {"name": "statement", "symbols": ["hexa"], "postprocess": d => ({ rule: "statement", children: d })},
-    {"name": "hexa$ebnf$1", "symbols": [/[0-9a-fA-F]/]},
-    {"name": "hexa$ebnf$1", "symbols": ["hexa$ebnf$1", /[0-9a-fA-F]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "hexa", "symbols": [{"literal":"$"}, "hexa$ebnf$1"], "postprocess": d => ({ rule: "hexa", value: d[1].join("") })}
-]
-  , ParserStart: "main"
-}
-if (typeof module !== 'undefined'&& typeof module.exports !== 'undefined') {
-   module.exports = grammar;
-} else {
-   window.grammar = grammar;
-}
+	function id(x) {
+		return x[0];
+	}
+	var grammar = {
+		Lexer: undefined,
+		ParserRules: [
+			{
+				name: "main",
+				symbols: ["statement"],
+				postprocess: d => ({ rule: "main", children: d })
+			},
+			{
+				name: "statement",
+				symbols: ["hexa"],
+				postprocess: d => ({ rule: "statement", children: d })
+			},
+			{ name: "hexa$ebnf$1", symbols: [/[0-9a-fA-F]/] },
+			{
+				name: "hexa$ebnf$1",
+				symbols: ["hexa$ebnf$1", /[0-9a-fA-F]/],
+				postprocess: function arrpush(d) {
+					return d[0].concat([d[1]]);
+				}
+			},
+			{
+				name: "hexa",
+				symbols: [{ literal: "$" }, "hexa$ebnf$1"],
+				postprocess: d => ({ rule: "hexa", value: d[1].join("") })
+			}
+		],
+		ParserStart: "main"
+	};
+	if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+		module.exports = grammar;
+	} else {
+		window.grammar = grammar;
+	}
 })();
